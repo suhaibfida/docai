4393cdc4484226d917dcdefee5fc9ad42127917c
suhaibfida <suhaibfida33@outlook.com>
2026-03-11 17:07:20 +0530

    add chunker and parser

---
+++ src/chunk.ts
@@ -0,0 +1,17 @@
+interface chunk {
+  text: string;
+}
+export const chunker = (data: chunk, chunkSize = 500, overlap = 100) => {
+  const text = data.text;
+
+  const chunks: string[] = [];
+
+  const split = text.split("/\s/");
+  let start = 0;
+  while (start < split.length) {
+    const process = split.slice(start, start + chunkSize).join("");
+    chunks.push(process);
+    start += chunkSize - overlap;
+  }
+  return chunks;
+};
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-console.log("docai");
+import { PDFParse } from "pdf-parse";
+import { chunker } from "./chunk.js";
+const text = async function run() {
+  const parser = new PDFParse({ url: "https://bitcoin.org/bitcoin.pdf" });
+  const result = await parser.getText();
+  chunker(result);
+  console.log(chunker(result));
+};
+text();
