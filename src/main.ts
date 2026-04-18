a0d218a6cc869424d13321ecd83352e3aa4d3248
suhaibfida <suhaibfida33@outlook.com>
2026-04-18 23:47:05 +0530

    add recursive

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { PDFParse } from "pdf-parse";
-import { chunker } from "./chunk.js";
+import { chunker } from "./textChunk/chunk.js";
 const text = async function run() {
   const parser = new PDFParse({ url: "https://bitcoin.org/bitcoin.pdf" });
   const result = await parser.getText();
   chunker(result);
-  console.log(chunker(result));
 };
 text();
--- src/textSplit/chunk.ts
+++ src/textSplit/chunk.ts
@@ -7,9 +7,11 @@ export const chunker = (data: chunk, chunkSize = 500, overlap = 100) => {
   const chunks: string[] = [];
 
   const split = text.split(/\s+/);
+  console.log(split);
   let start = 0;
   while (start < split.length) {
     const process = split.slice(start, start + chunkSize).join(" ");
+    console.log(process);
     chunks.push(process);
     start += chunkSize - overlap;
   }
